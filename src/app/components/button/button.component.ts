bdde20a65217952b7e484f6dd4db5e012fe3132a
Igor Napierala <igornapierala@gmail.com>
2022-05-11 23:17:46 +0200

    Functionality of emitting by button

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -10,8 +10,13 @@ export class ButtonComponent implements OnInit {
   text!: string;
   @Input()
   color!: string;
+  @Output() btnClick = new EventEmitter();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onClick() {
+    this.btnClick.emit();
+  }
 }
